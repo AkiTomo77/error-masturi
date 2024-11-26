88b594b289d823ee26ab69798ae61472b15e74f6
AkiTomo77 <144910488+AkiTomo77@users.noreply.github.com>
2024-11-26 01:42:37 +0000

    削除機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,15 @@
 class PostsController < ApplicationController
+	protect_from_forgery
 	def index
 		@posts = Post.all
 	end
 
   def new
-      @post = Post.new
+    @post = Post.new
   end
 
   def create
-      post = Post.new(post_params)
+    post = Post.new(post_params)
     if post.save!
       redirect_to :action => "index"
     else
@@ -16,8 +17,14 @@ class PostsController < ApplicationController
     end
   end
 
+	def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to action: :index
+  end
+
   private
   def post_params
-    params.require(:post).permit(:body, :name, :age, :user_id)
+    params.require(:post).permit(:name, :age, :body, :user_id)
   end
 end
